5af98f3f83058cfe51e87112ea251a0f806a834d
GaboxD7 <alej.gabriel.14@gmail.com>
2022-04-19 16:37:34 -0400

    INTERACTUAR DOM

--- js/app.js
+++ js/app.js
@@ -1,5 +1,170 @@
- import { stockProductos } from "./stock.js";
+// import { stockProductos } from "./stock.js";
+// INICIO DE PRODUCTOS
+const stockProductos = [
+    {
+        id: 1,
+        nombre: "Polera Manga Larga",
+        desc: "Polera con mangas largas, protecion al maximo.",
+        genero: "Mujer",
+        tipo: "Polera",
+        img: "img/mujer/poleraM-1.jpg",
+        precio: 15000,
+        talla: "M"
+    },
+    {
+        id: 2,
+        nombre: "Polera sin Manga",
+        desc: "Comodidad, frescura y siempre a la moda.",
+        genero: "Mujer",
+        tipo: "Polera",
+        img: "img/mujer/poleraM-2.jpg",
+        precio: 15000,
+        talla: "M"
+    },
+    {
+        id: 3,
+        nombre: "Sosten deportivo",
+        desc: "Sientete Segura en cada movimiento.",
+        genero: "Mujer",
+        tipo: "Polera",
+        img: "img/mujer/poleraM-3.jpg",
+        precio: 15000,
+        talla: "M"
+    },
+    {
+        id: 4,
+        nombre: "Polerra sin Manga",
+        desc: "Comodidad, frescura y siempre a la moda.",
+        genero: "Mujer",
+        tipo: "Polera",
+        img: "img/mujer/poleraM-4.jpg",
+        precio: 15000,
+        talla: "M"
+    },
+    
+    {
+        id: 5,
+        nombre: "Pantalon Lila",
+        desc: "Comodida y estilo.",
+        genero: "Mujer",
+        tipo: "Pantalon",
+        img: "img/mujer/PantalonM-1.jpg",
+        precio: 20000,
+        talla: "M"
+    },
+    {
+        id: 6,
+        nombre: "Pantalon Blanco",
+        desc: "Comodida y estilo.",
+        genero: "Mujer",
+        tipo: "Pantalon",
+        img: "img/mujer/PantalonM-2.jpg",
+        precio: 20000,
+        talla: "M"
+    },
+    {
+        id: 7,
+        nombre: "Pantalon Negro",
+        desc: "Comodida y estilo.",
+        genero: "Mujer",
+        tipo: "Pantalon",
+        img: "img/mujer/PantalonM-3.jpg",
+        precio: 20000,
+        talla: "M"
+    },
+    {
+        id: 8,
+        nombre: "Pantalon Gris",
+        desc: "Comodida y estilo.",
+        genero: "Mujer",
+        tipo: "Pantalon",
+        img: "img/mujer/PantalonM-4.jpg",
+        precio: 20000,
+        talla: "M"
+    },
+    {
+        id: 9,
+        nombre: "Polera sin Mangas",
+        desc: "Frescura, comodidad y tendencia.",
+        genero: "Hombre",
+        tipo: "Polera",
+        img: "img/hombre/poleraM-1.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 10,
+        nombre: "Polera sin Mangas",
+        desc: "Frescura, comodidad y tendencia.",
+        genero: "Hombre",
+        tipo: "Polera",
+        img: "img/hombre/poleraM-2.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 11,
+        nombre: "Polera Manga Larga",
+        desc: "Proteccion, comodidad.",
+        genero: "Hombre",
+        tipo: "Polera",
+        img: "img/hombre/poleraM-3.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 12,
+        nombre: "Polera con Mangas",
+        desc: "Frescura, comodidad y tendencia.",
+        genero: "Hombre",
+        tipo: "Polera",
+        img: "img/hombre/poleraM-4.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 13,
+        nombre: "Pantalon Negro",
+        desc: "Comodida y estilo.",
+        genero: "Hombre",
+        tipo: "Pantalon",
+        img: "img/hombre/pantalonM-1.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 14,
+        nombre: "Pantalon veis",
+        desc: "Comodida y estilo.",
+        genero: "Hombre",
+        tipo: "Pantalon",
+        img: "img/hombre/pantalonM-2.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 15,
+        nombre: "Pantalon corto",
+        desc: "Comodida y estilo.",
+        genero: "Hombre",
+        tipo: "Pantalon",
+        img: "img/hombre/pantalonM-3.jpg",
+        precio: 12000,
+        talla: "M"
+    },
+    {
+        id: 16,
+        nombre: "Pantalon slimfit",
+        desc: "Comodida y estilo.",
+        genero: "Hombre",
+        tipo: "Pantalon",
+        img: "img/hombre/pantalonM-4.jpg",
+        precio: 12000,
+        talla: "M"
+    }
+    ];
 
+// FIN DE PRODUCTOS
 
 let carro = [];
 let producto = {};
