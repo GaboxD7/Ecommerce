d55082fbf313159790302059b365c7c5c3a1211b
GaboxD7 <alej.gabriel.14@gmail.com>
2022-05-01 17:07:54 -0400

    add Funciones botnes, bootones-nav,  background

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 import { stockProductos } from "./stock.js";
-// import { carrito }  from "./carro.js";
 
-// FIN DE PRODUCTOS
-
+let stock = stockProductos;
 let carro = [];
+
 // let producto = {};
-
-
 
 const contenedor = document.querySelector("#contenedor");
 let carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');
 const  bageContar = document.querySelector("#badgeCount");
-
-
+ const mujer = document.querySelectorAll(".Mujer");
+ const hombre = document.querySelectorAll(".Hombre");
+ const nino = document.querySelectorAll(".nino");
+ const jumbo = document.querySelector("#jumbo");
+// let mujer = document.getElementsByClassName("Mujer")
+// const hombre = document.querySelector('#Hombre');
+const verTodo = document.querySelectorAll('.vertodo');
+// const nino = document.querySelector('#nino');
 
 class Carrito {
 
@@ -29,52 +32,26 @@ class Carrito {
 
 }
 
-
-function cards() {
-    stockProductos.forEach((prod) => {
-        const div = document.createElement("div");
-        div.className = "col-3 mb-3";
-        div.innerHTML = `
-                            <div class="card " >
-                                <div>
-                                    <a href=#! id="enlaceModal${prod.id}"><img src=${prod.img} class="card-img-top img-size  "   alt="..."></a>
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">${prod.nombre}</h5>
-                                    <p class="card-text">${prod.desc}.</p>
-                                </div>
-                                <ul class="list-group list-group-flush d.flex">
-                                    <li class="list-group-item">Genero:  ${prod.genero}</li>
-                                    <li class="list-group-item">Talla: ${prod.talla}</li>
-                                    <li class="list-group-item precio">Precio: $<span>${prod.precio} </span></li>
-            
-                                  
-                                </ul>
-                                <span>Cantidad:</span>
-                                <select id="cantidad${prod.id}" name"unidades">
-                                    <option value=1>1</option>
-                                    <option value=2>2</option>
-                                    <option value=3>3</option>
-                                    <option value=4>4</option>
-                                 </select>
-                             
-                                <button class="card-btnn add-carro" id="${prod.id}" data-id="${prod.id}" type="button">AGREGAR AL CARRITO</button>
-                            </div>`;
-        contenedor.appendChild(div);
-    });
-}
 cargarEventos()
 // Cargar Eventos 
 function cargarEventos() {
+// 
 
+mujer.forEach(item => item.addEventListener("click", cards))
+mujer.forEach(item => item.addEventListener("click", fondo))
+hombre.forEach(item => item.addEventListener("click", cards))
+nino.forEach(item => item.addEventListener("click", cards))
+verTodo.forEach(item => item.addEventListener("click", cards))
+// addEventListener('click', cards)
+// hombre.addEventListener('click', cards)
+// verTodo.addEventListener('click', cards)
+// nino.addEventListener('click', cards)
     // Add un producto "Agregar Carrito"
     contenedor.addEventListener('click', agregarProducto);
 
     // Eliminar
     carrito.addEventListener('click', eliminarProducto);
-    // bageContar.addEventListener('click', mostrar )
-
-
+   
     // Muestra los productos de Local Storage
     document.addEventListener('DOMContentLoaded', () => {
          carro = JSON.parse(localStorage.getItem('carrito')) || [];
@@ -94,11 +71,89 @@ function cargarEventos() {
 }
 
 
+function cards(e) {
+  
+   
+  
+    console.log(e.target.className)
+    console.log(verTodo[0].className)
+    if (e.target.className == verTodo[0].className  ) {
+        // console.log(e.target);
+        // console.log(verTodo)
+        contenedor.innerHTML = '';
+        // limpiarContenedorHTML();
+    
+        stock = stockProductos
 
+ 
+    } else  {
+        const exis = stock.some(prod => e.target.className !== prod.genero)
+        // console.log(e.target.className)
+        // console.log(stock)
+        // console.log(exis)
+        // console.log(prod)
+        if (exis) {
+            // limpiarContenedorHTML();
+            contenedor.innerHTML = '';
+            stock = stockProductos.filter(elem => elem.genero ==  e.target.className || elem.genero == e.target.classList.item(2))
+        }
+     
+    //  console.log(stock)
+    } 
+mostrarCard(stock)
+
+
+}
+function fondo () {
+    if (mujer[0].classList.contains("Mujer")) {
+        console.log("hola")
+        jumbo.classList.replace("jumbo", "jumbo-mujer")
+        console.log( mujer[0].classList)
+    }
+
+}
+
+
+
+function mostrarCard (stock) {
+
+
+    stock.forEach((prod) => {
+        const div = document.createElement("div");
+        div.className = "col-3 mb-3";
+        div.innerHTML = `
+                            <div class="card " >
+                                <div>
+                                    <a href=#! id="enlaceModal${prod.id}"><img src=${prod.img} class="card-img-top img-size  "   alt="..."></a>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">${prod.nombre}</h5>
+                                    <p class="card-text">${prod.desc}</p>
+                                </div>
+                                <ul class="list-group list-group-flush d.flex">
+                                    <li class="list-group-item">Genero:  ${prod.genero}</li>
+                                    <li class="list-group-item">Talla: ${prod.talla}</li>
+                                    <li class="list-group-item precio">Precio: $<span>${prod.precio} </span></li>              
+                                </ul>
+                                <span>Cantidad:</span>
+                                <select id="cantidad${prod.id}" name"unidades">
+                                    <option value=1>1</option>
+                                    <option value=2>2</option>
+                                    <option value=3>3</option>
+                                    <option value=4>4</option>
+                                 </select>
+                             
+                                <button class="card-btnn add-carro" id="${prod.id}" data-id="${prod.id}" type="button">AGREGAR AL CARRITO</button>
+                            </div>`;
+        contenedor.appendChild(div);
+    });
+}
 //  Agregar Producto
 
 function agregarProducto(e) {
+
     e.preventDefault();
+
     if (e.target.classList.contains('add-carro')) {
        const productoSeleccionado = e.target.parentElement;
     
@@ -111,6 +166,7 @@ function agregarProducto(e) {
 
 
 function eliminarProducto (e) {
+
     e.preventDefault();
 
     console.log(e.target.classList.contains('borrar-producto'))
@@ -122,6 +178,7 @@ function eliminarProducto (e) {
             mostrarCarroHtml()
             
     }
+
     if (carro.length == 0 ){
         bageContar.innerHTML = '';
     }
@@ -130,25 +187,25 @@ function eliminarProducto (e) {
 }
 
 function leerDatosProducto(producto) {
+
     const id = producto.querySelector('button').getAttribute('data-id');
     const imagen = producto.querySelector('img').src;
     const nombre = producto.querySelector('h5').textContent;
     const precio = producto.querySelector('ul  .precio span').textContent;
     let cantidad = parseInt(document.getElementById(`cantidad${id}`).value);
 
-
    // Creo el nuevo producto
-    const productoCarro = new Carrito(id, nombre, precio, imagen, cantidad )
+
+    const productoCarro = new Carrito(id, nombre, precio, imagen, cantidad );
 
     productoCarro.subtotal = Number(productoCarro.precio*productoCarro.cantidad);
 
-    // console.log(productoCarro)
+    const existe = carro.some(prod => prod.id == productoCarro.id);
 
-   const existe = carro.some(prod => prod.id == productoCarro.id);
-
-    // console.log(carro)
+  
     if(existe) {
                 // Actualizamos la cantidad
+
                 const productos = carro.map(producto =>  {
                     if (producto.id === productoCarro.id) {
                         producto.cantidad = cantidad + producto.cantidad;
@@ -171,8 +228,6 @@ function leerDatosProducto(producto) {
                        carro.push(productoCarro);    
             }
 
-      
-  
  mostrarCarroHtml()
 
 }
@@ -185,15 +240,16 @@ function leerDatosProducto(producto) {
    limpiarHTML()
   
   // Muestro en el html del carro
-    contenedorCarrito.innerHTML = "";
+
+     contenedorCarrito.innerHTML = "";
   
     carro.forEach((prod) => {
     const {imagen, nombre, precio, cantidad, subtotal, id} = prod;
           const tr = document.createElement("tr"); 
 
        tr.innerHTML = ` 
-     <tr>
-        <td><img src="${imagen}" height=40px class="w-100"</td>
+     <tr class="p-4">
+        <td><img src="${imagen}" height=65px class="w-100"</td>
         <th scope="row" class="text-center">${cantidad}</th>
         <td>${nombre}</td>
         <td>${precio}</td>
@@ -201,32 +257,47 @@ function leerDatosProducto(producto) {
         <td>
             <a href="#" class="borrar-producto text-center" data-id="${id}"> <img src="img/basura.png" class="img-trash  borrar-producto" > </a>
         </td>
-     </tr>
-     
-
-
-           `;
+     </tr>`;
   // add el html del carro en el tbody
        contenedorCarrito.appendChild(tr);
      });
 
     // Agregar el carrito de compras al storage
-     bageContar.className = "balge"
-     bageContar.innerHTML = carro.length;
 
+ 
      addCarroStorage()
+     mostrarNumeroCarrito()
    }
 
+   mostrarNumeroCarrito()
+   //
+   function mostrarNumeroCarrito () {
+ 
+   if (localStorage.getItem("carrito")) {
+    let karro = JSON.parse(localStorage.getItem('carrito'));
+    bageContar.className = "balge"
+    bageContar.innerHTML = karro.length;
+     
+   } else {
+       
 
-   
+   bageContar.innerHTML = "";
+    
+   }
+  
+
+
+   }
    // funcion para agreagr carro al estorage
+
 function addCarroStorage () {
+
     localStorage.setItem('carrito', JSON.stringify(carro));
+
 }
 
-
-
 function limpiarHTML() {
+
     // limpiar nuestro HTML
 
     while (contenedorCarrito.firstChild) {
@@ -236,10 +307,14 @@ function limpiarHTML() {
 }
 
 
-
 function totalGeneral() {
+
     let productoTotal = carro.reduce((total, producto) => total + producto.subtotal, 0);
 
     return productoTotal;
 }
-cards();
+window.addEventListener('click', (e) => {
+
+    e.preventDefault()
+})
+// cards();
--- js/stock.js
+++ js/stock.js
@@ -1,8 +1,8 @@
-const stockProductos = [
+let stockProductos = [
 {
     id: 1,
     nombre: "Polera Manga Larga",
-    desc: "Polera con mangas largas, protecion al maximo.",
+    desc: "Polera con mangas largas.",
     genero: "Mujer",
     tipo: "Polera",
     img: "img/mujer/poleraM-1.jpg",
@@ -12,7 +12,7 @@ const stockProductos = [
 {
     id: 2,
     nombre: "Polera sin Manga",
-    desc: "Comodidad, frescura y siempre a la moda.",
+    desc: "Comodidad y frescura.",
     genero: "Mujer",
     tipo: "Polera",
     img: "img/mujer/poleraM-2.jpg",
@@ -22,7 +22,7 @@ const stockProductos = [
 {
     id: 3,
     nombre: "Sosten deportivo",
-    desc: "Sientete Segura en cada movimiento.",
+    desc: "Sientete Segura y fresca.",
     genero: "Mujer",
     tipo: "Polera",
     img: "img/mujer/poleraM-3.jpg",
@@ -32,7 +32,7 @@ const stockProductos = [
 {
     id: 4,
     nombre: "Polerra sin Manga",
-    desc: "Comodidad, frescura y siempre a la moda.",
+    desc: "Comodidad y frecura.",
     genero: "Mujer",
     tipo: "Polera",
     img: "img/mujer/poleraM-4.jpg",
@@ -159,7 +159,170 @@ const stockProductos = [
     img: "img/hombre/pantalonM-4.jpg",
     precio: 12000,
     talla: "M"
-}
+},
+{
+    id: 17,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-1.jpg",
+    precio: 10000,
+    talla: "M"
+},
+{
+    id: 18,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 19,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-3.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 20,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-4.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 21,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Sweter",
+    img: "img/nino/sweter-o1.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 22,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Sweter",
+    img: "img/nino/sweter-o2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+
+{
+    id: 23,
+    nombre: "Short y Polera",
+    desc: "Pantalon deportivo niño.",
+    genero: "nino",
+    tipo: "Pantalon",
+    img: "img/nino/pantalon-o1.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 24,
+    nombre: "Short y Polera",
+    desc: "Pantalon deportivo niño.",
+    genero: "nino",
+    tipo: "Pantalon",
+    img: "img/nino/pantalon-o2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 25,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-a1.jpg",
+    precio: 10000,
+    talla: "M"
+},
+{
+    id: 26,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-a2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 27,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-a3.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 28,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Short y polera",
+    img: "img/nino/conjunto-a4.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 29,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Sweter",
+    img: "img/nino/sweter-a1.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 30,
+    nombre: "Short y Polera",
+    desc: "Conjunto deportivo de futbol.",
+    genero: "nino",
+    tipo: "Sweter",
+    img: "img/nino/sweter-a2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+
+{
+    id: 31,
+    nombre: "Short y Polera",
+    desc: "Pantalon deportivo niña.",
+    genero: "nino",
+    tipo: "Pantalon",
+    img: "img/nino/pantalon-a1.jpg",
+    precio: 10000,
+    talla: "S"
+},
+{
+    id: 32,
+    nombre: "Short y Polera",
+    desc: "Pantalon deportivo niña.",
+    genero: "nino",
+    tipo: "Pantalon",
+    img: "img/nino/pantalon-o2.jpg",
+    precio: 10000,
+    talla: "S"
+},
+
 ];
 
 
@@ -167,7 +330,6 @@ const stockProductos = [
 export  {
     stockProductos
 }
-
 
 
 
